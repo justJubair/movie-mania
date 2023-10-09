5b03a56d7a235ebe0e31e7e5ab811d7ae0c01d82
Jubair <jubair.ahmed2838@gmail.com>
2023-10-10 02:02:20 +0600

    Add: movie delete functionality added

--- src/components/SavedShows/SavedShows.jsx
+++ src/components/SavedShows/SavedShows.jsx
@@ -1,9 +1,10 @@
 import { MdChevronLeft, MdChevronRight } from "react-icons/md"
 import useAuth from "../../hooks/useAuth";
 import { useEffect, useState } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebase.config";
-
+import {AiOutlineClose} from "react-icons/ai"
+import toast from "react-hot-toast";
 const SavedShows = () => {
     const [movies, setMovies] = useState([])
     const {user} = useAuth()
@@ -21,6 +22,21 @@ const SavedShows = () => {
             setMovies(doc.data()?.savedShows)
         })
       },[user?.email])
+
+      const movieRef = doc(db, "users", `${user?.email}`)
+      const deleteShow = async (passedID)=>{
+        try{
+            const result = movies.filter(item=> item.id !== passedID)
+          await updateDoc(movieRef, {
+            savedShows: result
+          })
+          toast.success("Deleted")
+        }
+        catch(error){
+          console.error(error)
+          toast.error(error.messge)
+        }
+      }
   return (
     <div>
       <h3 className="text-gray-400 text-xl font-semibold p-4 mt-4">My Shows</h3>
@@ -33,7 +49,7 @@ const SavedShows = () => {
             <img src={`https://image.tmdb.org/t/p/w500${movie?.img}`} alt={movie?.title} />
             <div className="absolute top-0 w-full h-full hover:bg-black/80 hover:opacity-100 opacity-0 text-white">
               <p className="font-bold flex justify-center items-center text-xs md:text-sm h-full">{movie?.title}</p>
-             
+             <p onClick={()=> deleteShow(movie.id)} className="absolute top-3 right-5"><AiOutlineClose size={20}/></p>
             </div>
             </div>
           ))}
